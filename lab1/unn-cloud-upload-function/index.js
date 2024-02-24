2cdc51efd2c205111f01efcc8302ac12c19d44fc
Alexey Svistunov <ansvistunov@gmail.com>
2024-02-24 14:34:53 +0300

    lab 1 init

---
+++ lab1/unn-cloud-upload-function/index.js
@@ -0,0 +1,36 @@
+const parseMultipart = require('parse-multipart');
+const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+function extractFile(event) {
+  const boundary = parseMultipart.getBoundary(event.headers['Content-Type']);
+  const parts = parseMultipart.Parse(Buffer.from(event.body, 'base64'), boundary);
+  const [{ filename, data }] = parts;
+  return {
+    filename,
+    data
+  }
+};
+
+async function putObject(data, name) {
+  const REGION = "ru-central1";
+  const ENDPOINT = "https://storage.yandexcloud.net";
+  const s3Client = new S3Client({
+    region: REGION, endpoint: ENDPOINT, credentials: {
+      accessKeyId: process.env.accessKeyId,
+      secretAccessKey: process.env.secretAccessKey
+    }
+  });
+  const bucket = process.env.bucket;
+  const params = { Bucket: bucket, Key: name, Body: data };
+  const results = await s3Client.send(new PutObjectCommand(params));
+  return results;
+}
+
+module.exports.handler = async function (event, context) {
+  const { filename, data } = extractFile(event);
+  result = await putObject(data, filename);
+  return {
+    statusCode: 200,
+    body: 'File uploaded!',
+  };
+};
