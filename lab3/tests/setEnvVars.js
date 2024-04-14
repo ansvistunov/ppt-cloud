ebf6d3902b133f46a3743f509ad35b4056f7a3ef
Alexey Svistunov <ansvistunov@gmail.com>
2024-04-14 11:36:43 +0300

    add lab3 base tests

---
+++ lab3/tests/setEnvVars.js
@@ -0,0 +1,7 @@
+process.env.POSTGRES_HOST="rc1d-xxxxxxxxx.mdb.yandexcloud.net"
+process.env.POSTGRES_PORT=6432
+process.env.POSTGRES_DB="db1"
+process.env.POSTGRES_USER="db_user"
+process.env.POSTGRES_PASSWORD="xxxxxxxxx"
+
+
